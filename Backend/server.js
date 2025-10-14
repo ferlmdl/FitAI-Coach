d9a3c306579ba35b624665071c09d2d68bd4bae0
ferlmdl <131288252+ferlmdl@users.noreply.github.com>
2025-10-14 22:28:45 +0000

    integración index

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ import userRouter from './routes/users.js';
 import videosRouter from './routes/videos.js';
 import 'dotenv/config';
 import { supabase } from './lib/supabaseClient.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 
 const app = express();
@@ -20,7 +22,20 @@ async function testSupabaseConnection() {
 }
 
 testSupabaseConnection();
-
+app.engine(
+  "html",
+  engine({
+    extname: ".html",
+    layoutsDir: path.join(__dirname, '../Frontend/layouts'),
+    partialsDir: path.join(__dirname, '../Frontend/partials'),
+    defaultLayout: "principal",
+    helpers: {
+      json: function(context) {
+        return JSON.stringify(context);
+      }
+    }
+  })
+);
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
